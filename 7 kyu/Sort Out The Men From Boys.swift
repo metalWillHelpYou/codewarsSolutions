c35a4f4c18283163a771d46d7ec6983c8a84a0da
Daniil Krivonogov <comma.mount_0x@icloud.com>
2022-09-28 16:57:08 +0200

    summary

--- 7 kyu/Sort Out The Men From Boys.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-func menFromBoys(_ arr: [Int]) -> [Int] {
-    let newArr = Array(Set(arr))
-    return newArr.filter { $0 & 1 == 0 }.sorted() + newArr.filter { $0 & 1 != 0 }.sorted(by: >)
-}
